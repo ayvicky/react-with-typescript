0be17066e92ce7377757a166798e6b200afe87af
ayvicky <ayvicky1@gmail.com>
2021-06-15 15:44:14 +0500

    setup simple state

---
+++ src/TextField.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface Person {
+    firstName: string
+    lastName: string
+}
+
+interface Props {
+    text: string;
+    ok?: boolean;
+    total?: number;
+    fn?: () => void;
+    fn2?: (bob: string) => string;
+    obj?: {
+        f1: string
+    };
+    person: Person
+
+}
+
+export const TextField: React.FC<Props> = ({
+    person,
+    text
+}) => {
+    const [count, setCount] = React.useState(5);
+    return(
+        <div>
+            <input />
+        </div>
+    )
+}
