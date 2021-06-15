6a96e16e699838a7dd5e2ebdb39295f01b98867a
ayvicky <ayvicky1@gmail.com>
2021-06-15 17:49:30 +0500

    setup types for render-props

---
+++ src/TextField.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import {Counter} from './Counter';
+
+interface Person {
+    firstName: string
+    lastName: string
+}
+
+interface Props {
+    text: string;
+    ok?: boolean;
+    total?: number;
+    fn?: () => void;
+    fn2?: (bob: string) => string;
+    obj?: {
+        f1: string
+    };
+    person: Person;
+    handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+}
+
+interface TextNode {
+    text: string
+}
+
+export const TextField: React.FC<Props> = ({
+    handleChange
+}) => {
+    const [count, setCount] = React.useState<number | null | undefined | string>(5);
+    const [title, setTitle] = React.useState<TextNode>({text: 'hello'});
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const divRef = React.useRef<HTMLDivElement>(null);
+
+    setCount('hello');
+    return(
+        <div ref={divRef}>
+            <input ref={inputRef} onChange={e => {
+                e.preventDefault();
+            }}/>
+            <Counter>
+                {(count, setCount) => (
+                    <div>
+                        {count}
+                        <button onClick={() => setCount(count + 1)}>+</button>
+                    </div>
+                )}
+            </Counter>
+        </div>
+    )
+}
